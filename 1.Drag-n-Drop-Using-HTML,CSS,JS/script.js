45d424d346fdce603b5057ff35ad72c5297463b5
unknown <ashrithagowda666@gmail.com>
2024-02-11 14:47:56 +0530

    worked on drag and drop functionality and check order

--- 1.Drag-n-Drop-Using-HTML,CSS,JS/script.js
+++ 1.Drag-n-Drop-Using-HTML,CSS,JS/script.js
@@ -52,21 +52,58 @@ function createList() {
 ///////////////
 function dragStart() {
   //   console.log(`Event: DragStart`);
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
 }
 function dragEnter() {
   //   console.log(`Event: DragEnter`);
+  this.classList.add("over");
 }
 function dragLeave() {
   //   console.log(`Event: DragLeave`);
+  this.classList.remove("over");
 }
-function dragOver() {
+function dragOver(e) {
   //   console.log(`Event: DragOver`);
+  e.preventDefault();
 }
 function dragDrop() {
   //   console.log(`Event: Drop`);
+  const dragEndIndex = +this.getAttribute("data-index");
+  console.log("dragEndIndex", dragEndIndex);
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
 }
 ///////////////
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
 
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+////////////////
+
+// Check the order of list items
+function checkOrder() {
+  let correctOrder = true;
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+      correctOrder = false;
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
+  if (correctOrder) {
+    alert("Congratulations");
+  } else {
+    alert("try again");
+  }
+}
+///////////////
 function addEventListener() {
   const draggables = document.querySelectorAll(".draggable");
   const dragListItems = document.querySelectorAll(".draggable-list li");
@@ -82,3 +119,4 @@ function addEventListener() {
     item.addEventListener("dragleave", dragLeave);
   });
 }
+check.addEventListener("click", checkOrder);
