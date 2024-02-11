37168e68b5d9b846ec0355e7e8b7a7e51e7889fc
unknown <ashrithagowda666@gmail.com>
2024-02-11 12:23:42 +0530

    worked on random displaying of listitems

---
+++ 1.Drag-n-Drop-Using-HTML,CSS,JS/readme/readme.md
@@ -0,0 +1,13 @@
+### Help me to understand this please, by showing what map returns, what is a.sort and b.sort?
+
+```
+Help me to understand this please, by showing what  map returns, what is a.sort and b.sort?
+ .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+```
+
+![Image](./assets/Screenshot_1.png)
+
+- in code, for better understanding: replaced variable names
+
+![Image](./assets/Screenshot_2.png)
---
+++ 1.Drag-n-Drop-Using-HTML,CSS,JS/script.js
@@ -0,0 +1,48 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Mukesh Ambani",
+  "Gautam Adani",
+  "Shiv Nadar",
+  "Savitri Jindal and Family",
+  "Cyrus Poonawalla",
+  "Dilip Shanghvi",
+  "Kumar Birla",
+  "Kushal Pal Singh",
+  "Lakshmi Mittal",
+  "Radhakishan Damani",
+];
+
+//Store listItems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert List Items into DOM
+
+function createList() {
+  [...richestPeople]
+    //   created an object of arrays [{},{}]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    // creating sorted array [{},{},{}]===>
+    .sort((firstItem, secondItem) => firstItem.sort - secondItem.sort)
+    //take each item {} in [{},{},{}] and take its value, with item.value
+    .map((item) => item.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
+      listItem.innerHTML = `
+    
+    <span class="number">${index + 1}</span>
+    <div class="draggable" dragabble="true">
+    <p class="person-name">${person}</p>
+    <ion-icon name="menu"></ion-icon>
+    </div>
+    `;
+      listItems.push(listItem);
+      draggable_list.appendChild(listItem);
+    });
+}
